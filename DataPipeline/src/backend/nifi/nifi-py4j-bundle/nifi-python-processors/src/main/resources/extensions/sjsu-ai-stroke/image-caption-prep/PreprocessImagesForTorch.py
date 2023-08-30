f12944f7c22e722765f9839a437867130a6073a2
james94 <jamesmedel94@gmail.com>
2023-08-30 14:36:46 -0700

    Updated NiFi Python PreprocessImagesForTorch to account for ICPSR stroke data

--- DataPipeline/src/backend/nifi/nifi-py4j-bundle/nifi-python-processors/src/main/resources/extensions/sjsu-ai-stroke/image-caption-prep/PreprocessImagesForTorch.py
+++ DataPipeline/src/backend/nifi/nifi-py4j-bundle/nifi-python-processors/src/main/resources/extensions/sjsu-ai-stroke/image-caption-prep/PreprocessImagesForTorch.py
@@ -22,6 +22,7 @@ import torchvision.transforms as transforms
 import pickle5 as pickle
 
 import PIL
+import SimpleITK as sitk
 
 # from tqdm import tqdm
 from nifiapi.properties import PropertyDescriptor
@@ -32,13 +33,14 @@ from nifiapi.flowfiletransform import FlowFileTransform, FlowFileTransformResult
 # TODO (JG): Limitation in flow is flow file not passed to next processor until processor finishes work. This is with each processor like this
 
 # TODO (JG): Make this work for training and testing sets
+# TODO (JG): Check if 3D Voxels is too much for Asus ROG Zephyrus
 class PreprocessImagesForTorch(FlowFileTransform):
     class Java:
         implements = ['org.apache.nifi.python.processor.FlowFileTransform']
     class ProcessorDetails:
         version = '0.0.1-SNAPSHOT'
-        dependencies = ['pandas==1.3.5', 'pillow==10.0.0', 'pickle5==0.0.11', "torch", "torchvision", "torchaudio"]
-        description = 'Gets Flickr JPEG filepaths from the pandas csv dataframe in the flow file, loads each JPEG file as a PIL image and performs resizing, pixel to tensor conversion and normalization on each PIL image using pytorch'
+        dependencies = ['pandas==1.3.5', 'SimpleITK==2.2.1', 'pillow==10.0.0', 'pickle5==0.0.11', "torch", "torchvision", "torchaudio"]
+        description = 'Gets JPEG or NifTI filepaths from the pandas csv dataframe in the flow file, loads each JPEG or NifTI file as a PIL image and performs resizing, pixel to tensor conversion and normalization on each PIL image using pytorch'
         tags = ['sjsu_ms_ai', 'csv', 'PIL', 'jpeg', 'pytorch']
 
     def __init__(self, **kwargs):
@@ -55,9 +57,9 @@ class PreprocessImagesForTorch(FlowFileTransform):
             default_value=False,
             required = True,
         )
-        self.jpeg_data_type = PropertyDescriptor(
-            name = 'JPEG Dataset Name',
-            description = 'The name of the JPEG Dataset, currently supported: { flickr }.',
+        self.data_type = PropertyDescriptor(
+            name = 'Dataset Name',
+            description = 'The name of the Dataset, currently supported: { flickr, icpsr_stroke }.',
             default_value = "flickr",
             required=True
         )
@@ -80,7 +82,7 @@ class PreprocessImagesForTorch(FlowFileTransform):
             required=True
         )
         self.expected_norm_keys = ['mean', 'std']
-        self.descriptors = [self.prep_torch_imgs_dir, self.already_prepped, self.jpeg_data_type, self.resize, self.center_crop, self.normalization]
+        self.descriptors = [self.prep_torch_imgs_dir, self.already_prepped, self.data_type, self.resize, self.center_crop, self.normalization]
 
     def getPropertyDescriptors(self):
         return self.descriptors
@@ -94,7 +96,7 @@ class PreprocessImagesForTorch(FlowFileTransform):
         # read pre-trained model and config file
         self.prep_torch_imgs_dirpath = context.getProperty(self.prep_torch_imgs_dir.name).getValue()
         self.img_prep_already_done = self.str_to_bool(context.getProperty(self.already_prepped.name).getValue())
-        self.jpeg_data_name = context.getProperty(self.jpeg_data_type.name).getValue()
+        self.data_name = context.getProperty(self.data_type.name).getValue()
         self.resize_img = context.getProperty(self.resize.name).asInteger()
         self.center_crop_img = context.getProperty(self.center_crop.name).asInteger()
         self.mean_std_norm_json_str = context.getProperty(self.normalization.name).getValue()
@@ -116,7 +118,7 @@ class PreprocessImagesForTorch(FlowFileTransform):
         if self.img_prep_already_done:
             self.logger.info("Adding Prepped Torch Image filepaths to data df in prep_torch")
 
-            self.img_name_to_prep_img_map = [prep_torch_dir + os.sep + self.jpeg_data_name + "_" + str(i) + ".pk1" for i in range(len(img_cap_csv_data))]
+            self.img_name_to_prep_img_map = [prep_torch_dir + os.sep + self.data_name + "_" + str(i) + ".pk1" for i in range(len(img_cap_csv_data))]
             img_cap_csv_data["img_name_to_prep_img"] = self.img_name_to_prep_img_map
             self.logger.info("Retrieved Prepped Torch Image filepaths stored at : {}/".format(prep_torch_dir))
         else:
@@ -124,13 +126,15 @@ class PreprocessImagesForTorch(FlowFileTransform):
             for i in range(len(img_cap_csv_data)):
                 torch_imgname_to_prepimg = {}
                 # Load the image using PIL
-                if self.jpeg_data_name == "flickr":
+                if self.data_name == "flickr":
                     input_image = PIL.Image.open(img_cap_csv_data.natural_image.iloc[i])
                     img_name = os.path.basename(img_cap_csv_data.natural_image.iloc[i])
-                # elif self.jpeg_data_name == "atlas":
+                # elif self.data_name == "atlas":
                 #     input_image = sitk.ReadImage(img_cap_csv_data.train_t1w_raw.iloc[i], sitk.sitkFloat32)
-                # elif self.jpeg_data_name == "icpsr_stroke":
-                #     input_image = sitk.ReadImage(img_cap_csv_data.brain_dwi_orig.iloc[i], sitk.sitkFloat32)
+                elif self.data_name == "icpsr_stroke":
+                    input_voxel = sitk.ReadImage(img_cap_csv_data.brain_dwi_orig.iloc[i], sitk.sitkFloat32)
+                    input_image = sitk.GetArrayFromImage(input_voxel)
+                    img_name = os.path.basename(img_cap_csv_data.brain_dwi_orig.iloc[i])
 
                 # Extract mean & std dev from json
                 norm_dict = json.loads(self.mean_std_norm_json_str)
@@ -139,6 +143,7 @@ class PreprocessImagesForTorch(FlowFileTransform):
                         self.logger.error("The key '{}' is missing or incorrect in the JSON data.".format(norm_key))
 
                 # Perform Torch Preprocessing: Resize, CenterCrop, ToTensor, Normalize
+                # TODO (JG): Check if pytorch ordinary transforms can work with 3D data
                 preprocess = transforms.Compose([
                     transforms.Resize(self.resize_img),
                     transforms.CenterCrop(self.center_crop_img),
@@ -154,7 +159,7 @@ class PreprocessImagesForTorch(FlowFileTransform):
                 preprocessed_torch_bytes = pickle.dumps(torch_imgname_to_prepimg)
 
                 # Save the image name mapped torch preprocessed image
-                output_path = os.path.join(prep_torch_dir, self.jpeg_data_name + "_" + str(i) + ".pk1")
+                output_path = os.path.join(prep_torch_dir, self.data_name + "_" + str(i) + ".pk1")
                 self.logger.info("Torch Preprocessed Image pickle output_path = {}".format(output_path))
                 try:
                     with open(output_path, "wb") as file:
