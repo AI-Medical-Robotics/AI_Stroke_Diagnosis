a318feaad3d0d43e7796667f8d3f71bb17d3592e
james94 <jamesmedel94@gmail.com>
2023-09-09 11:30:24 -0700

    Updated skull strip seg train script w simple unet pred; testing

--- DataPipeline/src/backend/torch/skull-strip-seg/torch_model.py
+++ DataPipeline/src/backend/torch/skull-strip-seg/torch_model.py
@@ -11,10 +11,10 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True)
         )
@@ -27,7 +27,7 @@ class Down(nn.Module):
         super(Down, self).__init__()
         self.mpconv = nn.Sequential(
             nn.MaxPool3d(2),
-            DoubleConv(in_channels, out_channels)
+            DoubleConv(in_channels=in_channels, out_channels=out_channels)
         )
 
     def forward(self, x):
@@ -37,7 +37,7 @@ class Up(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Up, self).__init__()
         self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-        self.conv = DoubleConv(in_channels, out_channels)
+        self.conv = DoubleConv(in_channels=in_channels, out_channels=out_channels)
     
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -52,29 +52,42 @@ class Up(nn.Module):
         return self.conv(x)
     
 class UNet3D(nn.Module):
-    def __init__(self, in_channels, out_channels, init_features=32):
+    def __init__(self, in_channels=1, out_channels=1, init_features=64):
         super(UNet3D, self).__init__()
-        self.conv1 = DoubleConv(in_channels, init_features)
-        self.down1 = Down(init_features, init_features * 2)
-        self.down2 = Down(init_features * 2, init_features * 4)
-        self.down3 = Down(init_features * 4, init_features * 8)
-        self.down4 = Down(init_features * 8, init_features * 16)
-        self.up1 = Up(init_features * 16, init_features * 8)
-        self.up2 = Up(init_features * 8, init_features * 4)
-        self.up3 = Up(init_features * 4, init_features * 2)
-        self.up4 = Up(init_features * 2, init_features)
-        self.conv2 = nn.Conv3d(init_features, out_channels, kernel_size=1)
+        self.conv1 = DoubleConv(in_channels=in_channels, out_channels=init_features)
+        self.down1 = Down(in_channels=init_features, out_channels=init_features * 2)
+        self.down2 = Down(in_channels=init_features * 2, out_channels=init_features * 4)
+        self.down3 = Down(in_channels=init_features * 4, out_channels=init_features * 8)
+        self.down4 = Down(in_channels=init_features * 8, out_channels=init_features * 16)
+        self.up1 = Up(in_channels=init_features * 16, out_channels=init_features * 8)
+        self.up2 = Up(in_channels=init_features * 8, out_channels=init_features * 4)
+        self.up3 = Up(in_channels=init_features * 4, out_channels=init_features * 2)
+        self.up4 = Up(in_channels=init_features * 2, out_channels=init_features)
+        self.conv2 = nn.Conv3d(in_channels=init_features, out_channels=out_channels, kernel_size=1)
 
     def forward(self, x):
+        # Reshape input tensor to remove the extra dimension: for ex, [1, 64, 96, 128, 160, 1] now [64, 96, 128, 160, 1]
+        # x = x.squeeze(0)
+        print("input: x.shape = {}".format(x.shape))
         x1 = self.conv1(x)
+        print("out conv1: x1.shape = {}".format(x1.shape))
         x2 = self.down1(x1)
+        print("out down1: x2.shape = {}".format(x2.shape))
         x3 = self.down1(x2)
+        print("out down1: x3.shape = {}".format(x3.shape))
         x4 = self.down1(x3)
+        print("out down1: x4.shape = {}".format(x4.shape))
         x5 = self.down1(x4)
+        print("out down1: x5.shape = {}".format(x5.shape))
         x = self.up1(x5, x4)
+        print("out up1: x.shape = {}".format(x.shape))
         x = self.up1(x4, x3)
+        print("out up1: x.shape = {}".format(x.shape))
         x = self.up1(x3, x2)
+        print("out up1: x.shape = {}".format(x.shape))
         x = self.up1(x2, x1)
+        print("out up1: x.shape = {}".format(x.shape))
         x = self.conv2(x)
+        print("out up1: x.shape = {}".format(x1.shape))
         return x
 
--- DataPipeline/src/backend/torch/skull-strip-seg/train.py
+++ DataPipeline/src/backend/torch/skull-strip-seg/train.py
@@ -1,15 +1,22 @@
 import os
+import zmq
+import time
 import pandas as pd
+import pickle5 as pickle
+import threading
 import SimpleITK as sitk
+
+from typing import Tuple
 
 import torch
 import torch.nn as nn
+from torchvision import transforms
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 
 import torchmetrics
-from torchmetrics import Accuracy, IoU
+from torchmetrics import Accuracy
 from focal_loss.focal_loss import FocalLoss
 
 from torch_model import UNet3D
@@ -25,71 +32,158 @@ from torch_model import UNet3D
 # nifti_csv_data["intensity_norm"] = normalized raw_index
 # nifti_csv_data["mask_index"]
 
+def iou_score(pred, target):
+    intersection = torch.logical_and(pred, target).sum()
+    union = torch.logical_or(pred, target).sum()
+    iou = intersection / (union + 1e-7)
+    return iou
+
 # Load Skull Strip Seg dataset
+# class BrainMRIDataset(torch.utils.data.Dataset):
+#     def __init__(self, brain_voxel_list, brain_mask_list, batch_size):
+#         self.batch_size = batch_size
+#         self.nifti_csv_df = pd.DataFrame({"intensity_norm": brain_voxel_list, "mask_index": brain_mask_list})
+#         # do we need to shuffle the rows of the pandas df randomly?
+#         self.nifti_csv_df = self.nifti_csv_df.sample(frac=1).reset_index(drop=True)
+
+#     def __len__(self):
+#         return len(self.nifti_csv_df) // self.batch_size
+
+#     def __getitem__(self, idx):
+#         print("idx = {}".format(idx))
+#         print("idx*self.batch_size 1st half = {}".format(idx*self.batch_size))
+#         print("(idx+1)*self.batch_size 2nd half = {}".format((idx+1)*self.batch_size))
+#         # batch_nifti_df = self.nifti_csv_df.iloc[0*self.batch_size:self.batch_size]
+#         batch_nifti_df = self.nifti_csv_df.iloc[idx*self.batch_size:(idx+1)*self.batch_size]
+#         # TODO (JG): make sure dims match ResizeCropITK py processor
+#         # sitk to torch tensor dims (batch_size, channels, depth, height, width)
+#         voxel_batch = torch.zeros((self.batch_size, 1, 96, 128, 160)).float()
+#         mask_batch = torch.zeros((self.batch_size, 1, 96, 128, 160)).float()
+#         for i, nifti_df in batch_nifti_df.iterrows():
+#             print("batch_nifti_df index = {}".format(i))
+#             voxel = sitk.ReadImage(nifti_df["intensity_norm"])
+#             voxel = sitk.GetArrayFromImage(voxel)
+#             voxel = torch.from_numpy(voxel).float()
+#             # voxel = voxel.permute(3, 1, 2, 0) # switch the depth at 0 pos and channels at 3 pos
+#             voxel = voxel.unsqueeze(0) # add new batch dim at front
+#             # print("voxel shape = {}".format(voxel.shape))
+
+#             mask_voxel = sitk.ReadImage(nifti_df["mask_index"])
+#             mask_voxel = sitk.GetArrayFromImage(mask_voxel)
+#             mask_voxel = torch.from_numpy(mask_voxel).float()
+#             mask_voxel = mask_voxel.unsqueeze(0)
+#             # print("mask_voxel shape = {}".format(mask_voxel.shape))
+
+#             voxel_batch[i] = voxel
+#             mask_batch[i] = mask_voxel
+        
+#         return voxel_batch, mask_batch
+
 class BrainMRIDataset(torch.utils.data.Dataset):
-    def __init__(self, brain_voxel_list, brain_mask_list, batch_size):
-        self.batch_size = batch_size
+    def __init__(self, brain_voxel_list, brain_mask_list):
+        # self.batch_size = batch_size
         self.nifti_csv_df = pd.DataFrame({"intensity_norm": brain_voxel_list, "mask_index": brain_mask_list})
         # do we need to shuffle the rows of the pandas df randomly?
         self.nifti_csv_df = self.nifti_csv_df.sample(frac=1).reset_index(drop=True)
+        # setup transform
+        # self.transform = transform
 
     def __len__(self):
-        return len(self.nifti_csv_df) // self.batch_size
+        return len(self.nifti_csv_df)
 
-    def __getitem__(self, idx):
-        batch_nifti_df = self.nifti_csv_df.iloc[idx*self.batch_size:(idx+1)*self.batch_size]
-        # pytorch image tensor convention: (batch_size, channels, height, width, depth)
-        voxel_batch = torch.zeros((self.batch_size, 1, 128, 128, 96)).float()
-        mask_batch = torch.zeros((self.batch_size, 1, 128, 128, 96)).float()
-        for i, nifti_df in batch_nifti_df.iterrows():
-            voxel = sitk.ReadImage(nifti_df["intensity_norm"])
-            voxel = sitk.GetArrayFromImage(voxel)
-            voxel = torch.from_numpy(voxel).float()
-            voxel = voxel.unsqueeze(-1)
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        # print("idx = {}".format(idx))
 
-            mask_voxel = sitk.ReadImage(nifti_df["mask_index"])
-            mask_voxel = sitk.GetArrayFromImage(mask_voxel)
-            mask_voxel = torch.from_numpy(mask_voxel).float()
-            mask_voxel = mask_voxel.unsqueeze(-1)
+        # voxel_batch = torch.zeros((self.batch_size, 1, 96, 128, 160)).float()
+        # mask_batch = torch.zeros((self.batch_size, 1, 96, 128, 160)).float()
 
-            voxel_batch[i] = voxel
-            mask_batch[i] = mask_voxel
+        # sitk to torch tensor dims (channels, depth, height, width)
+        voxel = sitk.ReadImage(self.nifti_csv_df["intensity_norm"].iloc[idx])
+        voxel = sitk.GetArrayFromImage(voxel)
+        voxel = torch.from_numpy(voxel).float()
+        voxel = voxel.unsqueeze(0) # add new batch dim at front
+        # print("voxel shape = {}".format(voxel.shape))
+
+        mask_voxel = sitk.ReadImage(self.nifti_csv_df["mask_index"].iloc[idx])
+        mask_voxel = sitk.GetArrayFromImage(mask_voxel)
+        mask_voxel = torch.from_numpy(mask_voxel).float()
+        mask_voxel = mask_voxel.unsqueeze(0)
+        # print("mask_voxel shape = {}".format(mask_voxel.shape))
+
+            # voxel_batch[i] = voxel
+            # mask_batch[i] = mask_voxel
         
-        return voxel_batch, mask_batch
+        return voxel, mask_voxel
 
-def train_skull_strip_seg_model(nifti_csv_data, epochs=7):
+def train_skull_strip_seg_model(nifti_csv_data, epochs=3):
     X_train, X_val, y_train, y_val = train_test_split(nifti_csv_data["intensity_norm"].tolist(), nifti_csv_data["mask_index"].tolist(), test_size=0.1)
 
-    brain_train_dataset = BrainMRIDataset(X_train, y_train, batch_size=128)
-    brain_val_dataset = BrainMRIDataset(X_val, y_val, batch_size=128)
+    print("X_train len = {}".format(len(X_train)))
+    print("X_val len = {}".format(len(X_val)))
 
-    train_loader = DataLoader(brain_train_dataset, batch_size=1, shuffle=True)
-    val_loader = DataLoader(brain_val_dataset, batch_size=1, shuffle=False)
+    print("Creating brain train & val datasets")
+    
+    # train_transform = val_transform = transforms.Compose([
+    #     transforms.ToTensor()
+    # ])
 
-    # in_channels=3, out_channels=1 for skull vs non-skull
-    unet3d_model = UNet3D(in_channels=3, out_channels=1, init_features=32)
+    # brain_train_dataset = BrainMRIDataset(X_train, y_train, transform=train_transform)
+    # brain_val_dataset = BrainMRIDataset(X_val, y_val, transform=val_transform)
+
+    brain_train_dataset = BrainMRIDataset(X_train, y_train)
+    brain_val_dataset = BrainMRIDataset(X_val, y_val)
+
+    print("brain_train_dataset len = {}".format(len(brain_train_dataset)))
+    print("brain_val_dataset len = {}".format(len(brain_val_dataset)))
+
+    print("Creating brain train & val dataloaders")
+
+    train_loader = DataLoader(brain_train_dataset, batch_size=4, shuffle=True)
+    val_loader = DataLoader(brain_val_dataset, batch_size=4, shuffle=False)
+
+    brain_voxel_custom, mask_voxel_custom = next(iter(train_loader))
+
+    print("brain_voxel_custom shape = {}".format(brain_voxel_custom.shape))
+    print("mask_voxel_custom shape = {}".format(mask_voxel_custom.shape))
+
+    print("Creating Skull Strip Seg UNet3D model")
+
+    # in_channels=1 for 1 medical image modality T2-weighted; out_channels=1 for skull vs non-skull
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    unet3d_model = UNet3D(in_channels=1, out_channels=1, init_features=4).to(device)
+
+    print("Compiling UNet3D with Adam, FocalLoss, IoU, Accuracy")
 
     # compile model with Adam optimizer, focal loss, metrics
     optimizer = optim.Adam(unet3d_model.parameters(), lr=1e-1)
-    focal_criterion = FocalLoss(gamma=0.7)
-    accuracy = torchmetrics.Accuracy()
+    focal_criterion = FocalLoss(gamma=0.7).to(device)
+    accuracy = torchmetrics.Accuracy(task="binary").to(device)
     # (num_classes=2 for skull vs non-skull
-    iou_score = torchmetrics.IoU(num_classes=2)
+    # iou_score = torchmetrics.IoU(num_classes=2)
     metrics = [iou_score, accuracy]
 
     # Define desired variables for tracking best validation performance
     best_val_loss = float("inf")
     best_val_metric = float("-inf")
 
+    print("Training UNet3D across {} epochs".format(epochs))
+
     unet3d_model.train()
     step = 100
     for epoch in range(epochs):
+        print("Epoch {}: Train across batch_idx and brain_data and target from train_loader".format(epoch))
         for batch_idx, (brain_data, target) in enumerate(train_loader):
+            brain_data = brain_data.to(device)
+            target = target.to(device)
+            print("batch_idx = {}; brain_data len = {}; target len = {}".format(batch_idx, len(brain_data), len(target)))
+            
             optimizer.zero_grad()
             output = unet3d_model(brain_data)
             loss = focal_criterion(output, target)
             loss.backward()
             optimizer.step()
+
+        print("Running Evaluation")
 
         # validation
         unet3d_model.eval()
@@ -97,6 +191,9 @@ def train_skull_strip_seg_model(nifti_csv_data, epochs=7):
         val_metric = 0.0
         with torch.no_grad():
             for val_brain_data, val_target in val_loader:
+                val_brain_data = val_brain_data.to(device)
+                val_target = val_target.to(device)
+
                 val_output = unet3d_model(val_brain_data)
                 val_loss += focal_criterion(val_output, val_target)
                 val_iou += iou_score(val_output, val_target)
@@ -119,3 +216,104 @@ def train_skull_strip_seg_model(nifti_csv_data, epochs=7):
         step += 100
 
         unet3d_model.train()
+
+# References:
+# - ChatGPT: https://chat.openai.com/c/78252db9-df19-429d-8473-c2b5a46c38e5
+
+# Reference perplexity.ai for logic where subscriber sends confirmation to publisher when message is received:
+# https://www.perplexity.ai/search/cf3ca7f0-a7f2-4395-8952-67198b09aef7?s=u
+
+class ZMQSubscriber:
+    def __init__(self, zmq_socket_address="tcp://127.0.0.1:5556"):
+        self.zmq_socket_address = zmq_socket_address
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect(self.zmq_socket_address)
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
+        self.running = False
+        self.receive_thread = None
+        self.data_to_process = None # store received data
+
+    def start(self):
+        self.running = True
+        self.receive_thread = threading.Thread(target=self.receive_data)
+        self.receive_thread.start()
+
+    def stop(self):
+        self.running = False
+        if self.receive_thread:
+            self.receive_thread.join()
+
+    def receive_data(self):
+        while self.running:
+            try:
+                # Receive the DataFrame bytes
+                dataframe_bytes = self.socket.recv()
+
+                # Deserialize the DataFrame using pickle
+                received_dataframe = pickle.loads(dataframe_bytes)
+
+                print("received_dataframe.head = {}".format(received_dataframe.head()))
+
+                self.data_to_process = received_dataframe
+
+                print("Going to send confirmation to NiFi PutZMQ Publisher")
+                self.socket.send_string("Received")
+            except Exception as e:
+                print("Error receiving DataFrame: {}".format(e))
+
+    def __del__(self):
+        self.stop()
+        self.socket.close()
+
+def main():
+    zmq_subscriber = ZMQSubscriber()
+
+    nifti_csv_df = None
+    epochs = 7
+
+    try:
+        print("Subscribing to {} to receive SkullStripSeg Prep DataFrame...".format(zmq_subscriber.zmq_socket_address))
+        zmq_subscriber.start()
+
+        while True:
+            # check if there is data to process
+            if zmq_subscriber.data_to_process is not None:
+                nifti_csv_df = zmq_subscriber.data_to_process
+
+                # pass the data to the training thread
+                train_thread = threading.Thread(target=train_skull_strip_seg_model, args=(nifti_csv_df, epochs))
+                train_thread.start()
+
+                # reset the data_to_process to None to indicate processing is done
+                zmq_subscriber.data_to_process = None
+                print("Received data filepaths in pd df to train, finished training, exiting")
+                break
+
+            time.sleep(0.2) # sleep for 200 milliseconds
+
+    except KeyboardInterrupt:
+        print("Stopping the subscriber...")
+        zmq_subscriber.stop()
+
+def main_alt():
+    nifti_csv_df = pd.read_csv("skull_strip_seg_prep.csv")
+    train_skull_strip_seg_model(nifti_csv_df, epochs=3)
+
+def main_unet():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    unet3d_model = UNet3D(in_channels=1, out_channels=1, init_features=32).to(device)
+
+    x = torch.randn((1, 96, 128, 160)).to(device)
+    x = x.unsqueeze(0)
+
+    preds = unet3d_model(x)
+
+    print(x.shape)
+
+    assert preds.shape == x.shape
+
+if __name__ == "__main__":
+    # main()
+    # main_alt()
+    main_unet()
