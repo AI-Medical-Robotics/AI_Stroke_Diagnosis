40c77a7620a9fe7f96c68eebd98ab6d7feec69db
james94 <jamesmedel94@gmail.com>
2023-09-24 15:04:28 -0700

    Changed SimpleUNet to SimpleUNet3D and got it predicting on test data
    Still need to verify that I can train SimpleUNet3D for skull strip seg, then stroke lesion seg

--- DataPipeline/src/backend/torch/skull-strip-seg/torch_model.py
+++ DataPipeline/src/backend/torch/skull-strip-seg/torch_model.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
+import torch.nn.functional as F
+import monai
 
 # Reference perplexity.ai for pytorch skull strip seg model
 # https://www.perplexity.ai/search/cf74b6d5-9888-462b-9063-e90859bbf389
@@ -26,7 +29,7 @@ class Down(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Down, self).__init__()
         self.mpconv = nn.Sequential(
-            nn.MaxPool3d(2),
+            nn.MaxPool3d(kernel_size=2, stride=2),
             DoubleConv(in_channels=in_channels, out_channels=out_channels)
         )
 
@@ -51,6 +54,7 @@ class Up(nn.Module):
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
     
+# UNet3D doesnt work right now because of input to output size issues
 class UNet3D(nn.Module):
     def __init__(self, in_channels=1, out_channels=1, init_features=64):
         super(UNet3D, self).__init__()
@@ -91,3 +95,90 @@ class UNet3D(nn.Module):
         print("out up1: x.shape = {}".format(x1.shape))
         return x
 
+
+# NOTE: SimpleUNet3D works now on some test data
+# Referenced Aladdin's 2D UNet PyTorch tutorial video and adapted it for my 3D UNet:
+    # "PyTorch Image Segmentation Tutorial with U-NET: everything from scratch baby"
+    # https://youtu.be/IHq1t7NxS8k?si=OoExNDXHms8J3rB2
+class SimpleUNet3D(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, hidden_features=[64, 128, 256, 512]):
+        super(SimpleUNet3D, self).__init__()
+
+        self.upsampling3d = nn.ModuleList()
+        self.downsampling3d = nn.ModuleList()
+
+        self.pool3d = nn.MaxPool3d(kernel_size=2, stride=2)
+
+        # Downsampling encoder part of UNET
+        for feature in hidden_features:
+            self.downsampling3d.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # Upsampling decoder part of UNET
+        for feature in reversed(hidden_features):
+            # we do ConvTranspose2d for up, then DoubleConv for 2 convs
+            self.upsampling3d.append(
+                nn.ConvTranspose3d(
+                    feature*2, feature, kernel_size=2, stride=2
+                )
+            )
+            self.upsampling3d.append(DoubleConv(feature*2, feature))
+
+        self.bottleneck3d = DoubleConv(hidden_features[-1], hidden_features[-1]*2)
+
+        self.final_conv3d = nn.Conv3d(hidden_features[0], out_channels, kernel_size=1)
+
+
+    def forward(self, x):
+        skip_connections3d = []
+
+        # we loop for downsampling
+        for down3d in self.downsampling3d:
+            x = down3d(x)
+            # for skip connections, ordering is important here, first is highest resolution, last is smallest resolution
+            skip_connections3d.append(x)
+            x = self.pool3d(x)
+
+        x = self.bottleneck3d(x)
+
+        # now go backwards to make skip connections easier, reverse list, go with skip connections with highest res to lowest res
+        skip_connections3d = skip_connections3d[::-1]
+
+        # we loop taking steps of 2 for upsampling with DoubleConv
+        for idx in range(0, len(self.upsampling3d), 2):
+            # upsample with ConvTranspose2d
+            x = self.upsampling3d[idx](x)
+            # Divide idx by 2 to get just idx since we're doing step by 2 above
+            skip_connection = skip_connections3d[idx//2]
+
+            # Soln to cases when not divisible by 2 issue
+                # NOTE: in the UNET paper, they did cropping, but we'll do resizing for this issue
+            if x.shape != skip_connection.shape:
+                # we check x from outward part of upsampling, ifnequal resize our x using skip_connection resolutions just by channels, ignore h & w
+                print("x.shape = {}".format(x.shape))
+                print("skip_connection.shape = {}".format(skip_connection.shape))
+                print("skip_connection.shape[2:] = {}".format(skip_connection.shape[2:]))
+                # x = TF.resize(x, size=skip_connection.shape[2:])
+                x = F.interpolate(x, size=skip_connection.shape[2:], mode="trilinear", align_corners=False)
+                # monai_resize3d = monai.transforms.Resize(spatial_size=skip_connection.shape[2:])
+                # x = monai_resize3d(x)
+
+            print("Concatenating skip connections")
+            # Concatenate skip connections and add them along channel dimensions (ex: we have batch, channel, h, w)
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            # Then run through DoubleConv
+            x = self.upsampling3d[idx+1](concat_skip)
+
+        return self.final_conv3d(x)
+
+def test():
+    # batch, chs, h, w, z
+    x = torch.randn((3, 1, 161, 161, 161))
+    model = SimpleUNet3D(in_channels=1, out_channels=1)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+    assert preds.shape == x.shape
+
+if __name__ == "__main__":
+    test()
